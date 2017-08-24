be6f298ffa9cbf2aae3295fcacf82330c0f33a54
root <guilhermealbuquerquerc@gmail.com>
2017-08-24 16:53:17 -0300

    Dados em post e get

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,11 +1,47 @@
 const routes = (server) => {
-
-    server.get('/', (req, res, next) => {
+ 
+    server.get( '/', (req, res, next) => {  
+    
         res.send('Hello');
 
         next();
     });
+ 
+
+
+    server.get( 'categoria', (req, res, next) => {  
+        
+        res.send(['1', 'aduhas9dhs']);
+
+        next();
+    }); 
+ 
+    server.post( 'categoria', (req, res, next) => { 
+   
+    
+    const { name } = req.params;
+
+        res.send(name);
+        
+        next();
+    });
+
+    /*server.put( 'categoria', (req, res, next) => { 
+
+        res.send();
+        
+        next();
+    });
+
+    server.delete( 'categoria', (req, res, next) => { 
+
+        res.send();
+        
+        next();
+    });
+    */
 }
+
 
 
 module.exports = routes;
--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,15 @@ const routes = require('../http/routes');
 const cors = require('./cors');
 const server = restify.createServer();
 
+server.pre(cors.preflight);
+
+
+server.use(cors.actual);
+
+server.use(restify.plugins.bodyParser());
+
+
+
 
 routes(server);
 
